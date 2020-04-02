4039e692913c58d196b5cc6d80af8b4684c971af
Amit Tiwari <smilyamitt@gmail.com>
2020-04-02 16:56:02 +0200

    flask_app.py file modified

--- reviewScrapper/flask_app.py
+++ reviewScrapper/flask_app.py
@@ -5,7 +5,12 @@ from flask import Flask, render_template, request,jsonify
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
+import ssl
 import pymongo
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 app = Flask(__name__)  # initialising the flask app with the name 'app'
 
@@ -24,13 +29,16 @@ def index():
                 return render_template('results.html',reviews=reviews) # show the results to user
             else:
                 flipkart_url = "https://www.flipkart.com/search?q=" + searchString # preparing the URL to search the product on flipkart
-                uClient = uReq(flipkart_url) # requesting the webpage from the internet
+                uClient = uReq(flipkart_url, context=ctx) # requesting the webpage from the internet
                 flipkartPage = uClient.read() # reading the webpage
                 uClient.close() # closing the connection to the web server
+
                 flipkart_html = bs(flipkartPage, "html.parser") # parsing the webpage as HTML
+
                 bigboxes = flipkart_html.findAll("div", {"class": "bhgxx2 col-12-12"}) # seacrhing for appropriate tag to redirect to the product link
                 del bigboxes[0:3] # the first 3 members of the list do not contain relevant information, hence deleting them.
                 box = bigboxes[0] #  taking the first iteration (for demo)
+
                 productLink = "https://www.flipkart.com" + box.div.div.div.a['href'] # extracting the actual product link
                 prodRes = requests.get(productLink) # getting the product page from server
                 prod_html = bs(prodRes.text, "html.parser") # parsing the product page as HTML
@@ -76,5 +84,7 @@ def index():
             #return render_template('results.html')
     else:
         return render_template('index.html')
+
+
 if __name__ == "__main__":
-    app.run(port=8000,debug=True) # running the app on the local machine on port 8000
+    app.run(port=8000,debug=True)    # running the app on the local machine on port 8000
